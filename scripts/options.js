9170b13f1b1c4cb89a2869371a587c0bb2fdfa18
colin-foster <colin.patrick.foster@gmail.com>
2017-04-25 17:31:55 +0300

    Update options.js

--- scripts/options.js
+++ scripts/options.js
@@ -1,6 +1,6 @@
 var vblog={
         login:'foster',
-		replieLink:'den-svyatogo-patrika-prekrasnyi-den-dlya-togo-chtoby-poznakomitsya',
-		followTag:'ru--svoboda',
+		replieLink:'pechenki',
+		followTag:'ru--blokcheijn',
 
 };	
